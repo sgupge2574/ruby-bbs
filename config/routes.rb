e91379c32f9d92a02208ca420aea0f91d70d2ede
yuto YAMAGUCHI <23238474@edu.cc.saga-u.ac.jp>
2025-05-08 02:50:11 +0900

    編集、削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 # localhost:3000/posts
 # []はコントローラをつける.indexはlocalhost:3000/posts、その他はlocalhost:3000/posts/＊＊＊
-   resources :posts, only: [:index, :new, :create]  
+   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy] 
 
 
 end
